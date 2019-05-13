2cc13040e51af0d1b59f50881e76adb03b3ce9f9
seerosecode <keshalakemusic@gmail.com>
2019-05-13 09:14:21 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
+    if req.path.match("/")
+      resp.write "Hello, World!"
+    end
 
+    resp.finish
+  end
 end
